421d292df633d0c306a9b9e0a60bc036a6cfc1f1
Matthew Sojourner Newton <matt@mnewton.com>
2020-08-30 21:41:19 -0700

    Clean up, format, and modernize

--- ios-config-mode.el
+++ ios-config-mode.el
@@ -1,6 +1,15 @@
-;;; ios-config-mode.el --- edit Cisco IOS configuration files
+;;; ios-config-mode.el --- Edit Cisco IOS configuration files
 
 ;; Copyright (C) 2004 Noufal Ibrahim <nkv at nibrahim.net.in>
+;;
+;; Author: Noufal Ibrahim <nkv at nibrahim.net.in>
+;; Maintainer: Matthew Sojourner Newton <matt@mnewton.com>
+;; Created: 9 Nov 2010
+;; Homepage: https://github.com/mnewt/IOS-config-mode
+;; Keywords: extensions
+;; Package-Requires: ((emacs "24.3"))
+;; SPDX-License-Identifier: GPL 2.0
+;; Version: 0.5
 ;;
 ;; This program is not part of Gnu Emacs
 ;;
@@ -18,47 +27,44 @@
 ;; along with this program; if not, write to the Free Software
 ;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
+;;; Commentary:
+
+;; ios-config-mode is an Emacs major mode that helps to view and edit Cisco IOS
+;; (tm) configuration files.
+;;
+;; See the README for more information.
+
 ;;; Code:
 
+(defgroup ios-config nil
+  "View and edit Cisco IOS (tm) configuration files."
+  :group 'languages
+  :prefix "ios-config-")
+
 (defvar ios-config-mode-hook nil
-  "Hook called by \"ios-config-mode\"")
+  "Hook called by `ios-config-mode'.")
 
 (defvar ios-config-mode-map
-  (let 
-      ((ios-config-mode-map (make-keymap)))
+  (let ((ios-config-mode-map (make-keymap)))
     (define-key ios-config-mode-map "\C-j" 'newline-and-indent)
     ios-config-mode-map)
-  "Keymap for Cisco router configuration major mode")
-
-;; Font locking definitions. 
-(defvar ios-config-command-face 'ios-config-command-face "Face for basic router commands")
-(defvar ios-config-toplevel-face 'ios-config-toplevel-face "Face for top level commands")
-(defvar ios-config-no-face 'ios-config-no-face "Face for \"no\"")
-(defvar ios-config-ipadd-face 'ios-config-ipadd-face "Face for IP addresses")
+  "Keymap for Cisco router configuration major mode.")
 
 (defface ios-config-ipadd-face
-  '(
-    (t (:inherit font-lock-type-face))
-    )
-  "Face for IP addresses")
+  '((t (:inherit font-lock-type-face)))
+  "Face for IP addresses.")
 
-(defface ios-config-command-face 
-  '(
-    (t (:inherit font-lock-builtin-face))
-    )
-  "Face for basic router commands")
+(defface ios-config-command-face
+  '((t (:inherit font-lock-builtin-face)))
+  "Face for basic router commands.")
 
 (defface ios-config-toplevel-face
-  '(
-    (t (:inherit font-lock-keyword-face))
-    )
-  "Face for basic router commands")
+  '((t (:inherit font-lock-keyword-face)))
+  "Face for top level router commands.")
 
 (defface ios-config-no-face
-  '(
-    (t (:underline t))
-    )
-  "Face for \"no\"")
+  '((t (:underline t)))
+  "Face for \"no\" prefixed commands.")
 
 (defconst ios-config-font-lock-keywords
   `((,(concat "\\_<"
@@ -67,7 +73,7 @@
                      "object-group" "access-group" "cluster" "username"
                      "service-policy") t)
        "\\_>")
-     . ios-config-toplevel-face)
+     . 'ios-config-toplevel-face)
     (,(concat "\\_<"
        (regexp-opt '("alias" "boot" "card" "diagnostic" "enable" "hostname"
                      "logging" "service" "snmp-server" "version" "vtp" "names"
@@ -85,83 +91,78 @@
                      "message-length" "call-home" "password" "encrypted"
                      "privilege") t)
        "\\_>")
-     . ios-config-command-face)
+     . 'ios-config-command-face)
     ("\\<\\(no\\)\\>" . ios-config-no-face)
-    ("\\<\\([0-9]\\{1,3\\}\\.[0-9]\\{1,3\\}\\.[0-9]\\{1,3\\}\\.[0-9]\\{1,3\\}\\)\\>" . ios-config-ipadd-face))
+    ("\\<\\([0-9]\\{1,3\\}\\.[0-9]\\{1,3\\}\\.[0-9]\\{1,3\\}\\.[0-9]\\{1,3\\}\\)\\>"
+     . 'ios-config-ipadd-face))
 
-  "Font locking definitions for cisco router mode")
+  "Font locking definitions for cisco router mode.")
 
-;; Imenu definitions. 
+;; Imenu definitions.
 (defvar ios-config-imenu-expression
-  '(
-    ("Interfaces"        "^[\t ]*interface *\\(.*\\)" 1)
+  '(("Interfaces"        "^[\t ]*interface *\\(.*\\)" 1)
     ("VRFs"              "^ip vrf *\\(.*\\)" 1)
     ("Controllers"       "^[\t ]*controller *\\(.*\\)" 1)
     ("Routing protocols" "^router *\\(.*\\)" 1)
     ("Class maps"        "^class-map *\\(.*\\)" 1)
-    ("Policy maps"       "^policy-map *\\(.*\\)" 1)
-    ))
-  
+    ("Policy maps"       "^policy-map *\\(.*\\)" 1)))
+
 ;; Indentation definitions.
 (defun ios-config-indent-line ()
-  "Indent current line as cisco router config line"
+  "Indent current line as cisco router config line."
   (let ((indent0 "^interface\\|redundancy\\|^line\\|^ip vrf \\|^controller\\|^class-map\\|^policy-map\\|router\\|access-list\\|route-map")
-	(indent1 " *main-cpu\\| *class\\W"))
+        (indent1 " *main-cpu\\| *class\\W"))
     (beginning-of-line)
     (let ((not-indented t)
-	  (cur-indent 0))
+          (cur-indent 0))
       (cond ((or (bobp) (looking-at indent0) (looking-at "!")) ; Handles the indent0 and indent1 lines
-	     (setq not-indented nil
-		   cur-indent 0))
-	    ((looking-at indent1)
-	     (setq not-indented nil
-		   cur-indent 1)))
+             (setq not-indented nil
+                   cur-indent 0))
+            ((looking-at indent1)
+             (setq not-indented nil
+                   cur-indent 1)))
       (save-excursion ; Indents regular lines depending on the block they're in.
-	(while not-indented
-	  (forward-line -1)
-	  (cond ((looking-at indent1)
-		 (setq cur-indent 2
-		       not-indented nil))
-		((looking-at indent0)
-		 (setq cur-indent 1
-		       not-indented nil))
-		((looking-at "!")
-		 (setq cur-indent 0
-		       not-indented nil))
-		((bobp) 
-		 (setq cur-indent 0
-		       not-indented nil)))))
+        (while not-indented
+          (forward-line -1)
+          (cond ((looking-at indent1)
+                 (setq cur-indent 2
+                       not-indented nil))
+                ((looking-at indent0)
+                 (setq cur-indent 1
+                       not-indented nil))
+                ((looking-at "!")
+                 (setq cur-indent 0
+                       not-indented nil))
+                ((bobp)
+                 (setq cur-indent 0
+                       not-indented nil)))))
       (indent-line-to cur-indent))))
 
+(defvar ios-config-mode-syntax-table
+  (let ((table (make-syntax-table)))
+    (modify-syntax-entry ?_ "w" table) ;All _'s are part of words.
+    (modify-syntax-entry ?- "w" table) ;All -'s are part of words.
+    (modify-syntax-entry ?! "<" table) ;All !'s start comments.
+    (modify-syntax-entry ?\n ">" table) ;All newlines end comments.
+    (modify-syntax-entry ?\r ">" table)) ;All linefeeds end comments.
+  "Syntax table for cisco router mode.")
 
-;; Custom syntax table
-(defvar ios-config-mode-syntax-table (make-syntax-table) 
-  "Syntax table for cisco router mode")
+;;;###autoload
+(define-derived-mode ios-config-mode prog-mode "IOS Configuration"
+  "Major mode for editing Cisco IOS (tm) configuration files.
 
-(modify-syntax-entry ?_ "w" ios-config-mode-syntax-table) ;All _'s are part of words. 
-(modify-syntax-entry ?- "w" ios-config-mode-syntax-table) ;All -'s are part of words. 
-(modify-syntax-entry ?! "<" ios-config-mode-syntax-table) ;All !'s start comments. 
-(modify-syntax-entry ?\n ">" ios-config-mode-syntax-table) ;All newlines end comments.
-(modify-syntax-entry ?\r ">" ios-config-mode-syntax-table) ;All linefeeds end comments.
-
-;; Entry point
-(defun ios-config-mode  ()
-  "Major mode for editing Cisco IOS (tm) configuration files"
-  (interactive)
-  (kill-all-local-variables)
-  (set-syntax-table ios-config-mode-syntax-table)
-  (use-local-map ios-config-mode-map)
-  (set (make-local-variable 'font-lock-defaults) '(ios-config-font-lock-keywords))
-  (set (make-local-variable 'indent-line-function) 'ios-config-indent-line)
-  (set (make-local-variable 'comment-start) "!")
-  (set (make-local-variable 'comment-start-skip) "\\(\\(^\\|[^\\\\\n]\\)\\(\\\\\\\\\\)*\\)!+ *")
-  (setq imenu-case-fold-search nil)  
-  (set (make-local-variable 'imenu-generic-expression) ios-config-imenu-expression)
-  (imenu-add-to-menubar "Imenu")
-  (setq major-mode 'ios-config-mode
-	mode-name "IOS configuration")
-  (run-hooks ios-config-mode-hook))
-
+\\{ios-config-mode-map\}"
+  :syntax-table ios-config-mode-syntax-table
+  :after-hook ios-config-mode-hook
+  (setq-local indent-line-function 'ios-config-indent-line
+              comment-start "!"
+              comment-start-skip "\\(\\(^\\|[^\\\\\n]\\)\\(\\\\\\\\\\)*\\)!+ *"
+              imenu-case-fold-search nil
+              imenu-generic-expression ios-config-imenu-expression)
+  (font-lock-add-keywords 'ios-config-mode ios-config-font-lock-keywords)
+  (imenu-add-to-menubar "Imenu"))
+              
+;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.cfg\\'" . ios-config-mode))
 
 (provide 'ios-config-mode)
